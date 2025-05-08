25d0ab1c707022f077e243548e2e9db25c3d718f
Ali <aliyeganegi2002@gmail.com>
2025-05-08 12:22:07 +0330

    changes ip for npm

--- vite.config.js
+++ vite.config.js
@@ -11,16 +11,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    // server: {
-    //     host: '0.0.0.0', // or your IP directly: '192.168.0.167'
-    //     port: 5173,
-    //     cors: {
-    //         origin: 'http://192.168.0.167:8080',
-    //         credentials: true,
-    //     },
-    //     strictPort: true,
-    //     hmr: {
-    //         host: '192.168.0.167' // 👈 important for hot reload and asset access
-    //     }
-    // },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        cors: {
+            origin: 'http://192.168.1.112:8080',
+            credentials: true,
+        },
+        strictPort: true,
+        hmr: {
+            host: '192.168.1.112'
+        }
+    },
 });
